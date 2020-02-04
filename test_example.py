9be83480d3cdec2d07c6b6df4689edf37f3faba6
AidanWoolley <32900997+AidanWoolley@users.noreply.github.com>
2020-02-04 13:03:24 +0000

    Fix all flake8 errors with test example.

--- test_example.py
+++ test_example.py
@@ -1,6 +1,14 @@
+"""
+An example python file to show docstrings and minimal type hints.
+
+The maximum line length used in this project is 120 characters.
+Docstrings are in the google style.
+"""
+
+
 def fib(n: int) -> int:
     """
-    Calculate the nth fibonacci number in O(n), starting 0, 1, 1 (with 0 at index 0)
+    Calculate the nth fibonacci number in O(n), starting 0, 1, 1 (with 0 at index 0).
 
     Args:
         n: The index of the fibonacci number to calculate
@@ -17,45 +25,53 @@ def fib(n: int) -> int:
     f2 = 0
     f1 = 1
     f0 = 1
-    if n < 0: raise ValueError("Sequence indices must be at least 0")
-    elif n == 0: return 0
+    if n < 0:
+        raise ValueError("Sequence indices must be at least 0")
+    elif n == 0:
+        return 0
     # Start at 1 because we need to execute the loop once for n==2, etc
     for i in range(1, n):
         f0 = f1 + f2
         f2 = f1
         f1 = f0
     return f0
-  
-  
+
+
 def test_fib_not_int():
-    """Test that `fib` throws TypeError on non-int argument"""
+    """Test that `fib` throws TypeError on non-int argument."""
     try:
-      fib(0.45)
-    except TypeError as e:
-      assert True
-      return
+        fib(0.45)
+    except TypeError:
+        assert True
+        return
     assert False
+
 
 def test_fib_negative():
-    """Test that `fib` throws ValueError on negative argument"""
+    """Test that `fib` throws ValueError on negative argument."""
     try:
-      fib(-1)
-    except ValueError as e:
-      assert True
-      return
+        fib(-1)
+    except ValueError:
+        assert True
+        return
     assert False
 
+
 def test_fib_0():
-    """Test fib(0) == 0"""
+    """Test fib(0) == 0."""
     assert fib(0) == 0
 
+
 def test_fib_1():
-    """Test fib(1) == 1"""
+    """Test fib(1) == 1."""
     assert fib(1) == 1
+
 
 def test_fib_general():
     """
     Test the first few values from `fib` are correct.
+
     As long as they are, all further values must also be correct due to Python's arbitrary-precision int arithmetic.
     """
-    assert [fib(x) for x in range(2, 15)] == [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+    assert [fib(x) for x in range(2, 15)] == [
+        1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
