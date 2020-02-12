fce15889fa316002daf326a02f48977e14a11dcd
Mihai Berechet <mb2271@cam.ac.uk>
2020-02-12 14:59:36 +0000

    Initial evaluator

--- evaluation/tester.R
+++ evaluation/tester.R
@@ -1,5 +1,12 @@
 # TODO 
-# list files in testcases
-# source every one of them
 # gather the tests, apply them
 # build json
+
+test_files <- list.files(path="testcases")
+for (current_test_file in test_files) {
+    source(paste("testcases/", current_test_file, sep=""))
+
+    for (current_test in .tests) {
+        print(current_test())
+    }
+}
