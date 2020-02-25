8840e8305357d4b0c40ec122bb2b6ee2ef584db0
Mihai Berechet <mb2271@cam.ac.uk>
2020-02-25 11:41:40 +0000

    Added a test function taking two arguments

---
+++ evaluation/testcases/test_addition.R
@@ -0,0 +1,16 @@
+source("src/add_two.R")
+source("test_tools.R")
+
+.descriptions <- list(
+	add_two = "adds two numbers together"
+)
+
+testAddZero <- function() {
+    tdk_tested_name <- "add_two"
+    tdk_test_description <- "add one and zero together"
+
+    result <- tdk_run(add_two, a=1, b=0)
+    assert_equals(result, 1.0)
+    
+    return(tdk_return())
+}
